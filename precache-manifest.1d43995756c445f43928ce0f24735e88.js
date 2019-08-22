f913a77c73e4dbe463ef56e14e9eb8da8a8888d7
Przemysław Chmiel <vendrom@gmail.com>
2019-08-22 21:31:20 +0200

    Updates

---
+++ precache-manifest.1d43995756c445f43928ce0f24735e88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03a414af943eb1fc89513faaa38299a4",
+    "url": "/reactApp/index.html"
+  },
+  {
+    "revision": "db4596bc26dfe78a2d91",
+    "url": "/reactApp/static/css/main.8b527b5b.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/reactApp/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "db4596bc26dfe78a2d91",
+    "url": "/reactApp/static/js/main.5890d350.chunk.js"
+  },
+  {
+    "revision": "4f6bef0ec26ba2365171",
+    "url": "/reactApp/static/js/runtime~main.56b3556b.js"
+  }
+]);
